51f5604002cfb686e1f78722dbad81db175d4552
Michael Marchand <michaeldavidmarchand@gmail.com>
2019-05-12 23:48:32 -0600

    setting up database

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require './trail'
 
+configure :development do
+    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 get '/' do
     erb :home
 end
